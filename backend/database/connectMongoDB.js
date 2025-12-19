44316a433d7e9f9e1798cfeeefff6b27ebd37276
Mohammad Rimawi <mohammadrimawi47@gmail.com>
2025-12-19 20:35:25 +0200

    1- Project setup

---
+++ backend/database/connectMongoDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected: ${conn.connection.host}`);
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+export default connectMongoDB;
